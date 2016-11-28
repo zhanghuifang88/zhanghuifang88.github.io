b612bdbf9144ad66664ed3afeb1750fe482662a6
zhanghuifang88 <1063152838@qq.com>
2016-11-28 22:18:19 +0800

    简历

---
+++ mobile-resume/js/index.js
@@ -0,0 +1,36 @@
+;(function(){
+	
+	var H = document.documentElement.clientHeight;
+	var list = document.querySelector(".list");
+	var allLi = list.getElementsByTagName("li");
+	
+	for(var i=0; i <  allLi.length; i++){
+		fn(allLi[i]);
+	}
+	function fn(li){
+		MTouch(li).swipeUp(
+			function(){
+				MTween(li,{"translateY":-H},700,"easeIn",function(){
+					li.style.display = "none";
+					MTween(li,{"translateY":0},700,"easeIn");
+					if(li.nextElementSibling){
+						console.log(11);
+						li.nextElementSibling.style.display = "block";
+					}else {
+						list.firstElementChild.style.display = "block";
+						console.log(list.firstElementChild);
+						//li.nextElementSibling.style.display = "none";
+					}
+					
+					
+				});
+				//li.style.display = "none";
+				
+			}
+		);
+	
+	}
+	
+	
+	
+})();
